bfcfd1e9809a936eb32af020f665756cbc8c3bfb
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2023-01-14 22:35:59 +0600

    Intital Commit

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(80.0),
+          child: Image.asset('lib/images/banana.jpg'),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Text(
+            "We deliver groceries at your desired place.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Text(
+          "Fresh Items Available Everyday",
+          style: TextStyle(color: Colors.grey[600]),
+        ),
+        GestureDetector(
+          onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(
+            builder: (context) {
+              return const HomePage();
+            },
+          )),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            padding: const EdgeInsets.all(24),
+            child: const Text(
+              "Get Started",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const Spacer(),
+      ],
+    ));
+  }
+}
